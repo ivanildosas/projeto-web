57841ba3d547b6dacb36dd8d78e3576ca2f29d2e
Lucas O. Monteiro <lucasoliveiram@gmail.com>
2017-08-19 21:04:20 -0300

    commit inicial

---
+++ src/main/java/ctrl/IndexCtrl.java
@@ -0,0 +1,14 @@
+package ctrl;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ViewScoped
+@ManagedBean
+public class IndexCtrl {
+	
+	public void teste() {
+		
+	}
+
+}
